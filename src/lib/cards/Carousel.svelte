ccd3aeee3e4b4c096eb00b2a9848de47bb8463a9
Hugo Pomarede <71328298+tucopomarede@users.noreply.github.com>
2022-06-10 14:55:40 +0200

    carousel OK (#5)

---
+++ src/lib/cards/Carousel.svelte
@@ -0,0 +1,85 @@
+<script>
+    import Icon from 'mdi-svelte'
+    import { mdiChevronLeft } from '@mdi/js'
+    import { mdiChevronRight } from '@mdi/js'
+	import EventCard from '$lib/cards/EventCard.svelte';
+
+    export let events
+
+    let indexToShow = 0;
+
+    console.log("eventss: ", events)
+
+    function moveIndex(way) {
+        console.log("move index, ", way)
+        if (way==='prev' && indexToShow > 0)
+            indexToShow--;
+        else if (way==='next' && indexToShow < events.length)
+            indexToShow++;
+        console.log("inde:", indexToShow)
+    }
+
+</script>
+
+<div>
+
+    <button
+        class="float-left"
+        on:click={() =>moveIndex("prev")}
+    >
+    <Icon path={mdiChevronLeft} color="black" />
+
+    </button>
+    <button
+    class="float-right"
+    on:click={() => moveIndex("next")}
+    >
+    <Icon path={mdiChevronRight} color="black" />
+    </button>
+<ul class="flex overflow-x-auto gap-6 snap-x snap-mandatory">
+        <EventCard event={events[indexToShow]}/>
+        <EventCard event={events[indexToShow +1]}/>
+        <EventCard event={events[indexToShow +2]}/>
+    <!-- <li class="shrink-0 snap-center align-top inline-block text-center">
+        <img src={events[indexToShow].image[0].url} alt={"ok"}/>
+        <span class="block clear-left">
+            {events[indexToShow].name}
+        </span>
+    </li>
+    <li class="shrink-0 snap-center align-top inline-block text-center">
+        <img src={events[indexToShow+1].image[0].url} alt={"ok"}/>
+        <span class="block clear-left">
+            {events[indexToShow+1].name}
+        </span >
+    </li>
+    <li class="shrink-0 snap-center align-top inline-block text-center">
+        <img src={events[indexToShow+2].image[0].url} alt={"ok"}/>
+        <span class="block clear-left">
+            {events[indexToShow+2].name}
+        </span >
+    </li> -->
+</ul>
+
+
+</div>
+
+<style>
+    li {
+        width: 180px;
+    }
+    img {
+        float: left;
+        width:  180px;
+        height: 130px;
+        object-fit: cover;
+    }
+
+    ul {
+        overflow: hidden;
+    }
+
+    button {
+        height: 130px;
+    }
+
+</style>
--- src/routes/[destination].svelte
+++ src/routes/[destination].svelte
@@ -3,28 +3,57 @@
 	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
 	import { getEvents } from '../services/events.js';
+	import Carousel from '$lib/cards/Carousel.svelte'
+    import Icon from 'mdi-svelte'
+	import { mdiChevronDown } from '@mdi/js'
+	import { mdiChevronUp } from '@mdi/js'
 	import EventCard from '$lib/cards/EventCard.svelte';
 	import DestinationHeader from "../lib/DestinationHeader.svelte";
 	import Header from '../lib/Header.svelte'
 
+	// let Carousel; // for saving Carousel component class
+  	// let carousel; // for calling methods of the carousel instance
+
 
 	let events = [];
+	let eventExpanded = true;
+
 	let city = $page.params.destination;
 	let coords = {
 		latitude: 43.610769,
 		longitude: 3.876716
 	};
 
-	onMount(() => {
-		const params = {
-			city,
-			sort: 'date,asc'
-		};
-		getEvents(params).then((response) => {
-			events = response.data.events;
-			if (events && events[0]?.location) coords = events[0].location;
-		});
+	onMount(async () => {
+    // const module = await import('svelte-carousel');
+    // Carousel = module.default;
+	const city = $page.params.destination;
+	const params = {
+		city,
+		sort: 'date,asc'
+	};
+	await getEvents(params).then((response) => {
+		events = response.data.events;
+		console.log('response:', response)
+		if (events && events[0]?.location) coords = events[0].location;
 	});
+	console.log("events", events)
+	});
+
+	function toggleHeight(maxHeight) {
+    let e = document.getElementById("expand-events"); // e = the gray div
+
+	console.log("e ", e.style.height)
+    
+    if(e.style.height != '0px') {
+        e.style.height = '0px'; // height of one line: 20px
+		eventExpanded = false;
+    } else {
+        e.style.height = maxHeight + 'px';
+		eventExpanded = true;
+    }
+}
+
 
 	const flyTo = (event) => {
 		if (event.location) return (coords = event.location);
@@ -33,20 +62,51 @@
 <div class="flex">
 	<!--	 left panel, 50% page width-->
 	<!--	 the html in left panel is temporary, for test MapContainer component, and will need to be split out-->
-	<div class="w-1/2 bg-gray-50">
-		<Header/>
-		<div>
-			<DestinationHeader {city} />
-		</div>
-		<div class="grid grid-cols-3 gap-4 p-5">
+	<!-- <div class="w-1/2 bg-gray-50"> -->
+		<!-- <div class="grid grid-cols-3 gap-4 p-5">
 			{#if events}
 				{#each events as event}
 					<div on:click={flyTo(event)}>
 						<EventCard {event} />
 					</div>
-				{/each}
-			{/if}
+				{/each} -->
+	<div class="w-1/2 bg-gray-50">
+		<Header/>
+		<div>
+			<DestinationHeader {city} />
 		</div>
+			<!-- <svelte:component
+				let:showPrevPage
+				this={Carousel}
+				bind:this={carousel}
+				{...props}
+			>
+			<button
+			type="button"
+			slot="prev"
+			on:click={showPrevPage}
+			>
+			&LongLeftArrow;
+		</button>
+			{#each events as event}
+				<EventCard on:click={flyTo(event)} {event} />
+			{/each}
+			</svelte:component> -->
+			{#if events.length > 0}
+			<div>
+				<h1 style="width: 90%;" on:click={() => toggleHeight(150)} class="inline-block">Events</h1>
+				<div on:click={() => toggleHeight(190)} class="inline-block float-right">
+				{#if eventExpanded}
+				<Icon path={mdiChevronDown} color="black" />
+				{:else}
+				<Icon path={mdiChevronUp} color="black" />
+				{/if}
+				</div>
+			<div style="height: 190px;" id="expand-events" class="m-auto expandable">
+			<Carousel events={events}/>
+			</div>
+			</div>
+			{/if}
 	</div>
 
 	<!-- right panel, 50% page width-->
@@ -55,3 +115,23 @@
 		<MapContainer {coords} />
 	</div>
 </div>
+
+<style>
+
+.expandable {
+    padding-top: 10px;
+    /* overflow set to hidden to hide the expanded text */
+    overflow: hidden;
+    /* all style changes will ease-in-out for 1s */
+    -moz-transition: all 0.3s ease-in-out;
+    -ms-transition: all 0.3s ease-in-out;
+    -o-transition: all 0.3s ease-in-out;
+    -webkit-transition: all 0.3s ease-in-out;
+    transition: all 0.3s;
+}
+
+.icon-class {
+	display: inline-block;
+}
+
+</style>
