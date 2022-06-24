45f3f8acb4cd8c612c0fbc64e1df41639714b3e8
Hugo Pomarede <hugo.pomarede@epitech.eu>
2022-06-24 16:08:05 +0200

    browseall ok no filters

---
+++ src/lib/cards/BrowseAll.svelte
@@ -0,0 +1,105 @@
+<script>
+    import Icon from 'mdi-svelte';
+    import { mdiChevronLeft, mdiChevronRight, mdiMagnify } from '@mdi/js';
+	import { getEvents } from '../../services/events.js';
+    import EventCard from '$lib/cards/EventCard.svelte';
+    import { onMount } from 'svelte';
+    import { page } from '$app/stores';
+
+	export let cardClickCB;
+    export let backCarousel; // get back to Carousel
+    let size = 12; // nombre d'events demandé à chaque page
+    let pagination = 1;
+    let arrayPagination = Array(1, 2, 3);
+	let city = $page.params.destination;
+    let events = [];
+    let totalPages = 0;
+
+
+    const search = (city, startDate, endDate) => {
+		const params = {
+			city,
+            size,
+            page: pagination,
+			sort: 'date,asc'
+		};
+
+		if (startDate) params.startDateTime = startDate + 'T00:00:00z';
+		if (endDate) params.endDateTime = endDate + 'T23:59:59z';
+
+		getEvents(params).then((response) => {
+			events = response.data.events;
+            totalPages = response.data.page.totalPages;
+		});
+	};
+
+    const managePages = async (index) => {
+        console.log("manage pages ", index)
+        pagination = index;
+        
+        if (pagination < 3)
+            arrayPagination = Array(1, 2, 3)
+        else
+            arrayPagination = Array(pagination-1, pagination, pagination+1)
+
+        await search(city);
+        console.log("array ;", arrayPagination)
+    }
+
+	onMount(async () => {
+		await search(city);
+	});
+
+</script>
+
+
+<div>
+	<div class="flex">
+		<button on:click={() => backCarousel()} class="flex text-center btn-browse">
+			<Icon path={mdiChevronLeft} color="black" />
+			<span>Back</span>
+		</button>
+		<button class="flex ml-auto text-center btn-browse">
+			<span>Min price</span>
+		</button>
+		<button class="flex ml-auto text-center btn-browse">
+			<span>Max price</span>
+		</button>
+		<button class="flex ml-auto text-center btn-browse">
+			<Icon path={mdiMagnify} color="black" />
+			<span>Search</span>
+		</button>
+	</div>
+	<div class="flex flex-wrap pl-0">
+		{#each events as e, index}
+		<div class="browse-grid-element">
+			<EventCard event={e} on:click={cardClickCB(e)} />
+		</div>
+		{/each}
+
+	</div>
+    <div class="flex">
+        <div class="m-auto"> <!-- pagination -->
+            <button on:click={() => managePages(pagination-1)} class="w-6 h-6 border border-black rounded-sm mx-2">
+                <Icon path={mdiChevronLeft} color="black" />
+			</button>
+			{#each arrayPagination as b}
+			<button on:click={() => managePages(b)} class="w-6 h-6 border border-black rounded-sm mx-2">
+				{b}
+			</button>
+			{/each}
+            <button on:click={() => managePages(pagination+1)} class="w-6 h-6 border border-black rounded-sm mx-2">
+                <Icon path={mdiChevronRight} color="black" />
+			</button>
+		</div>
+    </div>
+</div>
+
+<style>
+
+	.browse-grid-element {
+		flex: 0 0 33.333333%;
+		padding: 10px;
+	}
+
+</style>
--- src/lib/cards/Carousel.svelte
+++ src/lib/cards/Carousel.svelte
@@ -1,21 +1,24 @@
 <script>
 	import Icon from 'mdi-svelte';
-	import { mdiChevronLeft, mdiChevronRight, mdiChevronUp, mdiChevronDown } from '@mdi/js';
+	import { mdiChevronLeft, mdiChevronRight, mdiChevronUp, mdiChevronDown, mdiMagnify } from '@mdi/js';
 	import EventCard from '$lib/cards/EventCard.svelte';
+	import BrowseAll from '$lib/cards/BrowseAll.svelte';
 
 	export let events;
 	export let title;
 	export let cardClickCB;
-	console.log('=>(Carousel.svelte:9) cardClickCB', cardClickCB.toString());
+	let BbrowseAll = false;
 
 	let indexToShow = 0;
 	let carouselExpanded = true;
+	console.log("events ", events.length)
+	
 
 	const toggleHeight = (maxHeight) => {
 		let e = document.getElementById('expand-events'); // e = the gray div
 
-		if (e.style.height != '0px') {
-			e.style.height = '0px'; // height of one line: 20px
+		if (e.style.height != '30px') {
+			e.style.height = '30px'; // height of one line: 20px
 			carouselExpanded = false;
 		} else {
 			e.style.height = maxHeight + 'px';
@@ -27,16 +30,42 @@
 		if (way === 'prev' && indexToShow > 0) indexToShow--;
 		else if (way === 'next' && indexToShow < events.length) indexToShow++;
 	};
+
+	const browseAll = () => {
+		console.log("browse all")
+		BbrowseAll = !BbrowseAll;
+	}
+
+	const movePagination = (index) => {
+		if (index > 1)
+			paginationLimitDown = (index-1) + (paginationIndex * index) - paginationIndex
+		else
+			paginationLimitDown = (index) + (paginationIndex * index) - paginationIndex
+		
+		paginationLimitUp = (index-1) + (paginationIndex * index)
+
+		indexActive = index;
+
+		console.log("page down ", paginationLimitDown, "page up ", paginationLimitUp)
+	}
 </script>
 
-<div id="expand-events" class="m-auto expandable">
-	<h3 on:click={() => toggleHeight(150)} class="inline-block">{title}</h3>
-	<div on:click={() => toggleHeight(300)} class="inline-block float-right">
-		{#if carouselExpanded}
-			<Icon path={mdiChevronDown} color="black" />
-		{:else}
-			<Icon path={mdiChevronUp} color="black" />
-		{/if}
+{#if !BbrowseAll}
+
+<div id="expand-events" class="m-auto expandable"> 
+	<div class="flex" style="height: 50px;">
+		<div on:click={() => toggleHeight(350)} class="inline-block float-right">
+			{#if carouselExpanded}
+				<Icon path={mdiChevronDown} color="black" />
+			{:else}
+				<Icon path={mdiChevronUp} color="black" />
+			{/if}
+		</div>
+		<h3 on:click={() => toggleHeight(350)} class="inline-block">{title}</h3>
+		<button on:click={() => browseAll()} class="flex text-center ml-auto btn-browse">
+			<Icon path={mdiMagnify} color="black" />
+			<span>Browse all</span>
+		</button>
 	</div>
 	<div class="flex items-center justify-center">
 		<button class="" on:click={() => moveIndex('prev')}>
@@ -45,13 +74,20 @@
 		<div class="flex overflow-x-auto gap-6 snap-x snap-mandatory">
 			<EventCard event={events[indexToShow]} on:click={cardClickCB(events[indexToShow])} />
 			<EventCard event={events[indexToShow + 1]} on:click={cardClickCB(events[indexToShow + 1])} />
-			<EventCard event={events[indexToShow + 2]} on:click={cardClickCB(events[indexToShow + 2])} } />
+			<EventCard event={events[indexToShow + 2]} on:click={cardClickCB(events[indexToShow + 2])} />
 		</div>
 		<button class="float-right" on:click={() => moveIndex('next')}>
 			<Icon path={mdiChevronRight} color="black" />
 		</button>
 	</div>
 </div>
+
+{:else}
+
+	<BrowseAll cardClickCB={cardClickCB} backCarousel={browseAll} />
+
+{/if}	
+
 
 <style>
 	.expandable {
@@ -64,5 +100,9 @@
 		-o-transition: all 0.3s ease-in-out;
 		-webkit-transition: all 0.3s ease-in-out;
 		transition: all 0.3s;
+	}
+
+	#btn-browse > * {
+		display: inline !important;
 	}
 </style>
